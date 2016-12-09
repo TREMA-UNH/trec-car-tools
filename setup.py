d98672d2b1f0de0ac20caec6394d995fc153169b
Laura Dietz <dietz@cs.umass.edu>
2016-12-09 11:27:34 -0500

    Setting up python project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='trec-car-tools',
+    version='1.0',
+    packages=['python3.5.read_data'],
+    url='trec-car.cs.unh.edu',
+    license='BSD 3-Clause',
+    author='laura-dietz',
+    author_email='Laura.Dietz@unh.edu',
+    description='Support tools for TREC CAR participants'
+)
